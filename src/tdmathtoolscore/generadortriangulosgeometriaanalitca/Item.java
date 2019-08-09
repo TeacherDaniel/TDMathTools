dfe06d37fb785775d709fe8abce2af3cd6cd753f
Daniel Saldana <consultor.daniel.saldana@gmail.com>
2019-08-08 19:05:12 -0500

    Adición de archivos requeridos para el generador de triángulos de Geometría Analítica

--- src/tdmathtoolscore/generadortriangulosgeometriaanalitca/Item.java
+++ src/tdmathtoolscore/generadortriangulosgeometriaanalitca/Item.java
@@ -5,10 +5,28 @@
  */
 package tdmathtoolscore.generadortriangulosgeometriaanalitca;
 
+import static java.lang.Math.*;
+
 /**
  *
  * @author Víctor Daniel
  */
 public class Item {
+    private final int _xp;
+    private final int _yp;
+    private final int _theta;
     
+    public double getM1(){
+        return tan(_theta);
+    }
+    
+    public double getTheta(){
+        return atan(getM1());
+    }
+    public Item(int xp, int yp,int theta){
+        _xp=xp;
+        _yp=yp;
+        _theta=theta;
+    }
+            
 }
